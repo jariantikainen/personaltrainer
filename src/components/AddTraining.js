0fc399ceb7104f87b44eccb525fc40de10933e55
jariantikainen <75965670+jariantikainen@users.noreply.github.com>
2021-01-13 02:55:59 +0200

    Task3

--- src/components/AddTraining.js
+++ src/components/AddTraining.js
@@ -6,6 +6,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import AddIcon from '@material-ui/icons/Add';
